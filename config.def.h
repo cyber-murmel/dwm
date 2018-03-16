4a6910ea2eb1b3ec05ac3440db4006dcb3d323ee
marble <marble@chaoscomputer.club>
2018-03-16 19:26:03 +0800

    keepassx at last tag

--- config.def.h
+++ config.def.h
@@ -30,7 +30,7 @@ static const Rule rules[] = {
 	{ "Gimp",           NULL,       NULL,       0,            1,           -1 },
 	{ "Thunderbird",    NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "Firefox",        NULL,       NULL,       1 << 2,       0,           -1 },
-	{ "Keepassx",       "keepassx", NULL,       1 << 3,       0,           -1 },
+	{ "Keepassx",       "keepassx", NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
