2f61eb82e6849241d89012c9a45e4e82e4833bc8
marble <marble@chaoscomputer.club>
2018-03-16 19:24:50 +0800

    simpler ip retrieval

--- status.sh
+++ status.sh
@@ -4,11 +4,6 @@
 VOL="$MUTE $(pamixer --get-volume)"
 LOCALTIME=`date '+%Z=%a %Y-%m-%d %H:%M'`
 OTHERTIME=`TZ=Europe/Berlin date +%Z\=%H:%M`
+IP=`ip r | grep kernel | awk '{match($1, /\/[0-9]*/); printf "%s: %s%s", $3, $NF, substr($1, RSTART, RLENGTH)}'`
 
-LINKS=`ip l | grep 'state UP' | awk '{print $2}' | sed 's/.$//'`    # get all active links
-IPS=$(for link in $LINKS; {                                         # iterate over all active links
-    echo -n $link: $(                                               # print link name
-    for l in `ip a show dev $link`; { echo $l; } |                  # get address of link
-    grep -Pe '^inet$' -A1 | tail -n1) " "; })                       # filter for IPv4 address
-
-echo " $IPS | $VOL | $OTHERTIME | $LOCALTIME "
+echo " $IP | $VOL | $OTHERTIME | $LOCALTIME "
