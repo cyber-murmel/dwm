93485724f6cddabcfedac978df01bde42916d43e
marble <marble@chaoscomputer.club>
2018-03-16 22:12:59 +0800

    battery status

--- status.sh
+++ status.sh
@@ -4,6 +4,7 @@
 VOL="$MUTE $(pamixer --get-volume)"
 LOCALTIME=`date '+%Z=%a %Y-%m-%d %H:%M'`
 OTHERTIME=`TZ=Europe/Berlin date +%Z\=%H:%M`
-IP=`ip r | grep kernel | awk '{match($1, /\/[0-9]*/); printf "%s: %s%s", $3, $NF, substr($1, RSTART, RLENGTH)}'`
+BAT=`for BAT in /sys/class/power_supply/BAT*; do  basename $BAT; cat $BAT/capacity; done | tr '\n' ' '`
+IP=`ip r | grep kernel | awk '{match($1, /\/[0-9]*/); printf "%s: %s%s ", $3, $NF, substr($1, RSTART, RLENGTH)}'`
 
-echo " $IP | $VOL | $OTHERTIME | $LOCALTIME "
+echo " $IP| $VOL | $BAT| $OTHERTIME | $LOCALTIME "
