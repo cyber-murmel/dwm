60c3ee3aca0759be2006bdccf4994715f3fbdedc
marble <marble@chaoscomputer.club>
2018-03-22 11:38:17 +0800

    change battery name

--- status.sh
+++ status.sh
@@ -4,7 +4,7 @@
 VOL="$MUTE $(pamixer --get-volume)"
 LOCALTIME=`date '+%Z=%a %Y-%m-%d %H:%M'`
 OTHERTIME=`TZ=Europe/Berlin date +%Z\=%H:%M`
-BAT=`for BAT in /sys/class/power_supply/BAT*; do  basename $BAT; cat $BAT/capacity; done | tr '\n' ' '`
+BAT=`for BAT in /sys/class/power_supply/axp288_fuel_gauge*; do  basename $BAT; cat $BAT/capacity; done | tr '\n' ' '`
 IP=`ip r | grep kernel | awk '{match($1, /\/[0-9]*/); printf "%s: %s%s ", $3, $NF, substr($1, RSTART, RLENGTH)}'`
 
 echo " $IP| $VOL | $BAT| $OTHERTIME | $LOCALTIME "
