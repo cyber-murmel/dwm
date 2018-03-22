cf9fd54fbbf0c046661893a42d19095adbab821b
marble <marble@chaoscomputer.club>
2018-03-22 11:51:24 +0800

    remove volume status; better BAT string

--- status.sh
+++ status.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/bash
 
 [[ $(pamixer --get-mute) = true ]] && MUTE="🔇" || MUTE="🔊"
-VOL="$MUTE $(pamixer --get-volume)"
 LOCALTIME=`date '+%Z=%a %Y-%m-%d %H:%M'`
 OTHERTIME=`TZ=Europe/Berlin date +%Z\=%H:%M`
-BAT=`for BAT in /sys/class/power_supply/axp288_fuel_gauge*; do  basename $BAT; cat $BAT/capacity; done | tr '\n' ' '`
+BAT="BAT $(cat /sys/class/power_supply/axp288_fuel_gauge/capacity)";
 IP=`ip r | grep kernel | awk '{match($1, /\/[0-9]*/); printf "%s: %s%s ", $3, $NF, substr($1, RSTART, RLENGTH)}'`
 
-echo " $IP| $VOL | $BAT| $OTHERTIME | $LOCALTIME "
+echo " $IP| $BAT| $OTHERTIME | $LOCALTIME "
+
